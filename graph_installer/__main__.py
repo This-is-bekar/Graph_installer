c983ad95b4e191678ee66971a521c57c520e3437
sekret666 <66795778+sekret666@users.noreply.github.com>
2021-01-24 15:18:44 +0400

    Update __main__.py

--- graph_installer/__main__.py
+++ graph_installer/__main__.py
@@ -25,7 +25,7 @@ def connect (api):
     return heroku_conn
 
 def createApp (connect):
-    appname = "asena" + str(time() * 1000)[-4:].replace(".", "") + str(random.randint(0,500))
+    appname = "graph" + str(time() * 1000)[-4:].replace(".", "") + str(random.randint(0,500))
     try:
         connect.create_app(name=appname, stack_id_or_name='container', region_id_or_name="eu")
     except requests.exceptions.HTTPError:
@@ -59,7 +59,7 @@ async def botlog (String, Api, Hash):
     await Client.start()
 
     KanalId = await Client(CreateChannelRequest(
-        title='AsenaUserBot BotLog',
+        title='GraphUserBot BotLog',
         about=LANG['AUTO_BOTLOG'],
         megagroup=True
     ))
